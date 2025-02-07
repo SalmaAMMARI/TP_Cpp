a5238a2b5d0f6f35a6f37bd6ab3fe7215d273011
SalmaAMMARI <salma.AMMARI@um6p.ma>
2025-02-07 20:36:13 +0000

    commit

---
+++ tp1_SalmaAMMARI/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream> //inclusion de la bibliothèque iostream
+#define PI 3.14  // définition de la constante PI
+#define DEBUG 1  //enlever le "//" avant define DEBUG PI si vous voulez tester le cas dont lequel DEBUG est définis
+
+
+int main() {
+
+    #ifdef DEBUG
+        std::cout<<"The macro DEBUG is  defined";
+    #else
+        std::cout<<"The macro DEBUG is not Defined";
+    #endif
+
+    return 0;
+}
